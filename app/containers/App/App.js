1e7792128cf78ac3ac9106c3e654430d79e3b3e3
James Hall <greatwolf3d@gmail.com>
2019-09-15 00:17:32 -0700

    initail commit and added styled components

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -15,20 +15,13 @@ import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
-import './style.scss';
+// import './style.scss';
 
 const App = () => (
-  <div className="app-wrapper">
-    <Helmet
-      titleTemplate="%s - React.js Boilerplate"
-      defaultTitle="React.js Boilerplate"
-    >
-      <meta name="description" content="A React.js Boilerplate application" />
-    </Helmet>
+  <div>
     <Header />
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/features" component={FeaturePage} />
       <Route path="" component={NotFoundPage} />
     </Switch>
     <Footer />
--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -33,10 +33,6 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
 
     return (
       <article>
-        <Helmet>
-          <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
-        </Helmet>
         <div className="home-page">
           <section className="centered">
             <h2>Start your next react project in seconds</h2>
