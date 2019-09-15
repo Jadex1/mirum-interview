ee1b3c4cdc1bc4fbbe02d9d9f2c836964fb5739f
James Hall <greatwolf3d@gmail.com>
2019-09-15 00:42:26 -0700

    added some files and removed some files

---
+++ app/containers/DetailPage/DetailPage.js
@@ -0,0 +1,15 @@
+/**
+ * DetailPage
+ * Will show the details of the gallery image
+ *
+ */
+
+import React from 'react';
+
+export default function DetailPage() {
+    return (
+        <article>
+            <h1>Display an image here.</h1>
+        </article>
+    );
+}
